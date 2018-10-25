d582245beb86657639efe9fc72447fd31f0efb46
Nursultan <nabakirov@GMAIL.COM>
2018-10-25 22:19:56 +0600

    mo gssm

--- mo/algorithms_py/gssm.py
+++ mo/algorithms_py/gssm.py
@@ -1,8 +1,6 @@
 
 def f(x):
-
-    return round(-20.04 * x ** 3 + x ** 2 + 2*x -1, 4)
-    # return round(-0.04 * X ** 3 + X ** 2 + X -1, 4)
+    return round(-0.04 * x ** 3 + x ** 2 + x -1, 4)
 
 a = -10
 b = 3
@@ -10,17 +8,20 @@ tol = 0.1
 k_max = 6
 
 import sys
-sys.stdout = open("./iterations/nurlan_gssm.txt", "w+")
+sys.stdout = open("./iterations/nurs2_gssm.txt", "w+")
+import math
 
 def gssm(a=a, b=b, tol=tol, k_max=k_max):
-	r = (5 ** (1/2)) / 2
+	r = (math.sqrt(5) - 1) / 2
 	x1 = a + (1 - r) * (b - a)
 	f1 = f(x1)
 	x2 = a + r * (b - a)
 	f2 = f(x2)
 	k = 0
 	print(f"Iteration {k}\n"\
-		  f"\tf(x) = -0.04 * X ^ 3 + X ^ 2 + X - 1\n"
+		  f"\tf(x) = -0.04 * X ^ 3 + X ^ 2 + X - 1\n"\
+		  f"\ta = {a}\n"\
+		  f"\tb = {b}\n"\
 		  f"\tr = (5 ^ (1/2)) / 2 = {r}\n"\
 		  f"\tx1 = a + (1-r) * (b-a) = {x1}\n"\
 		  f"\tf1 = f(x1) = {f1}\n"\
@@ -44,7 +45,7 @@ def gssm(a=a, b=b, tol=tol, k_max=k_max):
 				  f"   x1 = x2 = {x1}\n"\
 				  f"   f1 = f2 = {f1}\n"\
 				  f"   x2 = a + r * (b-a) = {x2}\n"\
-				  f"   f2 = f(x2) = {f2}\n\n")
+				  f"   f2 = f(x2) = {f2}")
 		else:
 			b = x2
 			x2 = x1
@@ -55,7 +56,8 @@ def gssm(a=a, b=b, tol=tol, k_max=k_max):
 				  f"   x2 = x1 = {x2}\n"\
 				  f"   f2 = f1 = {f2}\n"\
 				  f"   x1 = a + (1-r) * (b-a) = {x1}\n"\
-				  f"   f1 = f(x1) = {f1}\n\n")
+				  f"   f1 = f(x1) = {f1}")
+		print(f"4) k < k_max -> {k} < {k_max}\t{k<k_max}\n\n")
 	print(f"x1 = {x1}\n"\
 		  f"f1 = {f1}\n"\
 		  f"k = {k}\n"\
